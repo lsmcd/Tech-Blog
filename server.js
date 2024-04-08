ab8b51fc00c0dd7a78e7bb6a6bd6e26c5c0c3a16
Logan McDaniel <lsmcd@pm.me>
2024-04-08 15:17:20 -0600

    add views

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const routes = require("./controllers");
 
-const express = require("./express");
+const express = require("express");
+
+const exphbs = require("express-handlebars");
 
 const app = express();
 
 const PORT = process.env.PORT || 3001;
 
+const hbs = exphbs.create();
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 
 app.use(routes);
 
