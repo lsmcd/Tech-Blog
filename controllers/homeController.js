ef98e90317aef7bcad50a80a873d3650dd4c4edc
Logan McDaniel <lsmcd@pm.me>
2024-04-08 16:51:57 -0600

    add login routes

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,7 +1,21 @@
 const router = require("express").Router();
 
+const withAuth = require("../utils/auth");
+
 router.get("/", (req, res) => {
     res.render("homepage");
+});
+router.get("/login", (req, res) => { 
+    res.render("login");
+});
+router.get("/signUp", (req, res) => { 
+    res.render("signUp");
+});
+router.get("/signIn", (req, res) => { 
+    res.render("signIn");
+});
+router.get("/dashboard", withAuth, (req, res) => {
+    res.render("dashboard");
 });
 
 module.exports = router;
